daa45b63de860c926d1c3a21dcecfe15e62cee6b
scoot <scootiemalootie@gmail.com>
2023-08-16 23:37:11 +1000

    Coded basic gameplay loop and beginning of player questions. Need to implement playing card duplicate checking and saving of cards dealt - scoot.

--- app/scripts/dealer.js
+++ app/scripts/dealer.js
@@ -7,10 +7,48 @@ function dealCard(){
     //  Create a new playing card 
     const playingCard = new PlayingCard();  
 
-    //  Increment the number of cards dealt
-    numCardsDealt += 1;
-
     //  Return the new playing card
     return playingCard;
+
+}
+
+/**
+ * Handles interaction with the player (including asking questions and checking answers)
+ * @param {num} value the number of cards dealt -1 (Used to determine which question to ask)
+ */
+function playerInteraction(value){
+
+    //  If the player has not answered all the questions
+    if (value < questions.length){
+
+        //  Ask the next question
+        console.log(questions[value]);  
+
+    }
+    
+    //  Get the answer from the player
+    response = prompt();
+
+    //  -----   Check the answer    -----
+
+    //  If the first question is being answered
+    if(value = 0){
+
+        if (response = "lower" && cardsDealt[1] > cardsDealt[0]){
+
+            playerLoss = true;
+
+        } else if (response = "higher" && cardsDealt[1] < cardsDealt[0]){
+
+            playerLoss = true;
+
+        }
+
+    //  If the second question is being answered
+    } else if (value = 1){
+
+        
+
+    }
 
 }
--- app/scripts/gameManager.js
+++ app/scripts/gameManager.js
@@ -1,3 +1,6 @@
+//  Initialise variable to represent whether the player has lost or not
+let playerLoss = false;
+
 //  Initialise variable to store the numbers of playing cards dealt
 //  (Used to name variables new playing cards are assigned to)
 let numCardsDealt = 0;
@@ -5,6 +8,9 @@ let numCardsDealt = 0;
 //  Initialise a list of playing cards that have been dealth
 //  (Used to stop duplicate cards being drawn)
 let cardsDealt = [];
+
+//  Store the questions to ask the player as a list
+let questions = ["Higher or lower?", "Inside or outside?", "Colour?", "Suit?"];
 
 //  When the webpage loads, intialise required listeners
 window.onload = function() {initialiseListeners()};
@@ -18,9 +24,31 @@ function initialiseListeners(){
     //  Initialise listener for the link that initiates the game when clicked
     document.querySelector("#gameInitiator").addEventListener("click", function() {  
 
-        //  Deal the first playing card
-        dealCard();
+        //  Run the game when the game initiator link is clicked
+        runGame();
         
     })
 
 }
+
+/**
+ * Runs the game. 
+ * (Deals cards from the deck until 5 cards have been pulled)
+ */
+function runGame(){
+
+    //  Continue the game until 5 cards have been dealt
+    while (numCardsDealt < 5 && !playerLoss){
+
+        //  Deal a playing card
+        dealCard();
+
+        //  Increment the number of cards dealt
+        numCardsDealt += 1;
+
+        //  Interact with player
+        playerInteraction(numCardsDealt -1);        
+
+    }   
+
+};
